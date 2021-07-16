9d2d19db882f350d72f7ae33a52dad509c1f998b
Alina Varkki <varkkia99@gmail.com>
2021-07-16 23:52:22 +0100

    questions can now be passed to the quiz from the full book screen, need to make an api call to get questions from the database

--- client/components/sections/bookPageSections/CoverButtonsSection.tsx
+++ client/components/sections/bookPageSections/CoverButtonsSection.tsx
@@ -10,6 +10,34 @@ interface CoverButtonsSection {
 
 const CoverButtonsSection = ({imgUrl, navigation}: CoverButtonsSection & NavigationInjectedProps) => {
 
+    const allQuestions = [
+        {
+            question: "What’s the biggest planet in our solar system?",
+            options: ["Jupiter","Saturn","Neptune","Mercury"],
+            correct_option: "Jupiter"
+        },
+        {
+            question: "What attraction in India is one of the famus in the world?",
+            options: ["Chand Minar","Taj Mahal","Stadium"],
+            correct_option: "Taj Mahal"
+        },
+        {
+            question: "What land animal can open its mouth the widest?",
+            options: ["Alligator","Crocodile","Baboon","Hippo"],
+            correct_option: "Hippo"
+        },
+        {
+            question: "What is the largest animal on Earth?",
+            options: ["The African elephant","The blue whale","The sperm whale","The giant squid"],
+            correct_option: "The blue whale"
+        },
+        {
+            question: "What is the only flying mammal?",
+            options: ["The bat","The flying squirrel","The bald eagle","The colugo"],
+            correct_option: "The bat"
+        }
+    ]
+
     return (
         <View>
             <View style={styles.coverAndQuestions}>
@@ -18,7 +46,7 @@ const CoverButtonsSection = ({imgUrl, navigation}: CoverButtonsSection & Navigat
                     <TouchableOpacity
                         style={styles.button}
                         onPress={() => {
-                            navigation.navigate("BookQuiz")
+                            navigation.navigate("BookQuiz", {allQuestions: allQuestions})
                             console.log("clicked")
                         }}>
                         <Text>Take a quiz</Text>
