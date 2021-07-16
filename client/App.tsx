863c75aa44d91c82c361537b54ff25f2b2317a68
Niklavs Meiers <niklavs.meiers@gmail.com>
2021-07-16 20:41:07 +0100

    Working on the list of books

--- client/App.tsx
+++ client/App.tsx
@@ -1,28 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { getAllEbooks } from './api/BooksApi';
+import BookCard from './components/BookCard';
 
 export default function App() {
 
   useEffect(() => {
-    getBooks().then(r => {});
+    loadBooks().then(r => {});
+    // getBooks().then(r => {});
   })
+  const [books, setBooks] = useState<any[]>([]);
+
+  const loadBooks = async () => {
+    let books: any = []
+
+    let book: any = {}
+    book["id"] = "1"
+    book["name"] = "name1"
+    book["description"] = "description1"
+    books.push(book);
+
+    book = {}
+    book["id"] = "2"
+    book["name"] = "name2"
+    book["description"] = "description2"
+    books.push(book);
+
+    book = {}
+    book["id"] = "3"
+    book["name"] = "name3"
+    book["description"] = "description3"
+    books.push(book);
+
+    setBooks(books);
+
+  }
 
   const getBooks = async () => {
     try {
       const responseJSON = await getAllEbooks("Sapiens: A Brief History of Humankind");
-      console.log(responseJSON);
+      let books = responseJSON.items;
+      let size = books.length;
+
+      for(let i = 0; i < size; i++) {
+        console.log(responseJSON.items[i].volumeInfo.title);
+      }
     } catch (error) {
 
     }
   }
 
+  const bookRender = ({item}: any) => (
+    <BookCard
+        name={item.name}
+        description={item.description}
+    />
+  )
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text>Open up App.tsx to start working on your app!!!</Text>
-
+      <BookCard name="name" description="description"/>
+      <FlatList
+          data={books}
+          keyExtractor={item => item.id.toString()}
+          renderItem={bookRender}/>
     </View>
   );
 }
---
+++ client/components/BookCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+export default function BookCard({name, description}: any) {
+
+    return (
+        <SafeAreaView>
+            <View style={styles.container}>
+                <Text>{name}</Text>
+                <Text>{description}</Text>
+            </View>
+        </SafeAreaView>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        // flex: 1,
+        backgroundColor: '#aaffd5',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 330,
+        height: 180,
+        marginBottom: 20
+    },
+});
