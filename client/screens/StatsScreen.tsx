a199ad2f2d4bb65cfb228ae40fa887a6baa471f0
Alina Varkki <varkkia99@gmail.com>
2021-07-18 02:12:00 +0100

    working on achievements screen, added rating for categories

--- client/screens/StatsScreen.tsx
+++ client/screens/StatsScreen.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
-import {Text, View} from "react-native"
+import { ScrollView, Text, View } from "react-native"
 import Screen from '../components/Screen';
 import AchievementsHeader from '../components/sections/achievementsSections/AchievementsHeader';
 import AchievementsBadgeSection from '../components/sections/achievementsSections/AchievementsBadgeSection';
+import AchievementsCategoryScores from '../components/sections/achievementsSections/AchievementsCategoryScores';
+import ChampionsBoard from '../components/sections/achievementsSections/ChampionsBoard';
 
 const StatsScreen = () => {
 
     return (
         <Screen>
-            <AchievementsHeader/>
-            <AchievementsBadgeSection/>
+            <ScrollView>
+                <AchievementsHeader/>
+                <AchievementsBadgeSection/>
+                <AchievementsCategoryScores/>
+                <ChampionsBoard/>
+            </ScrollView>
         </Screen>
     )
 }
